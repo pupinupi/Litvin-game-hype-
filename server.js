b52dd6b122480719235fbfbfc8d7af888238078c
pupinupi <cool.lg-lg@yandex.ru>
2026-03-01 15:40:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,84 @@
-const express=require("express");
-const http=require("http");
-const {Server}=require("socket.io");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
-const app=express();
-const server=http.createServer(app);
-const io=new Server(server);
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.use(express.static("public"));
 
-const players={};
+const rooms = {};
 
-io.on("connection",(socket)=>{
+io.on("connection", socket => {
 
-players[socket.id]={
+socket.on("joinRoom", ({name,roomCode,color})=>{
+
+if(!rooms[roomCode]){
+rooms[roomCode]={
+players:[],
+turn:0
+};
+}
+
+const room=rooms[roomCode];
+
+const player={
 id:socket.id,
+name,
+color,
 position:0,
 hype:0
 };
 
-socket.emit("init",players[socket.id]);
+room.players.push(player);
 
-socket.on("rollDice",()=>{
+socket.join(roomCode);
 
-const player=players[socket.id];
-if(!player)return;
+io.to(roomCode).emit("roomUpdate",room);
+
+});
+
+/* ===== DICE ===== */
+
+socket.on("rollDice",(roomCode)=>{
+
+const room=rooms[roomCode];
+if(!room) return;
 
 const dice=Math.floor(Math.random()*6)+1;
 
+const player=
+room.players[room.turn];
+
+if(!player) return;
+
+/* движение */
 player.position+=dice;
+player.position%=21;
+
+/* HYPE */
 player.hype+=dice*5;
 
-if(player.hype>100)
-player.hype=100;
-
-io.emit("diceRolled",{
-id:player.id,
-dice,
-position:player.position,
-hype:player.hype
-});
-
+/* победа */
 if(player.hype>=100){
-io.emit("winner",player.id);
+io.to(roomCode)
+.emit("gameOver",player);
+return;
 }
-});
 
-socket.on("disconnect",()=>{
-delete players[socket.id];
+/* следующий ход */
+room.turn++;
+room.turn%=room.players.length;
+
+io.to(roomCode).emit(
+"diceResult",
+{
+dice,
+room
+}
+);
+
 });
 
 });
