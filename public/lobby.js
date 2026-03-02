be1408dd6417024bb5dc3253ef01fdc8db325d95
pupinupi <cool.lg-lg@yandex.ru>
2026-03-02 11:55:46 +0300

    Create lobby.js

---
+++ public/lobby.js
@@ -0,0 +1,16 @@
+const socket=io();
+
+function join(){
+
+localStorage.name=name.value;
+localStorage.room=room.value;
+localStorage.color=color.value;
+
+socket.emit("joinRoom",{
+name:name.value,
+room:room.value,
+color:color.value
+});
+
+location="game.html";
+}
