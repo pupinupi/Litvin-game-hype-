17f651f131c00c18111cbb060510cda3681b4eba
pupinupi <cool.lg-lg@yandex.ru>
2026-03-03 13:55:21 +0300

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,33 +1,33 @@
+console.log("FINAL MAP OK");
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 1024;
 
-let points = [];
+/* ===== ИГРОК ===== */
 
-function drawPoints(){
-  ctx.clearRect(0,0,1024,1024);
+let player = {
+  pos: 0,
+  hype: 0
+};
 
-  ctx.fillStyle="red";
-  points.forEach(p=>{
-    ctx.beginPath();
-    ctx.arc(p.x,p.y,10,0,Math.PI*2);
-    ctx.fill();
-  });
-}
+/* ===== ТВОИ ТОЧНЫЕ КООРДИНАТЫ ===== */
 
-canvas.addEventListener("click",function(e){
+const path = [
 
-  const rect = canvas.getBoundingClientRect();
+{ x:89,  y:599, type:"start" },
+{ x:88,  y:459, type:"+3" },
+{ x:80,  y:366, type:"+2" },
+{ x:86,  y:244, type:"scandal" },
+{ x:95,  y:133, type:"risk" },
 
-  const x = Math.round((e.clientX - rect.left));
-  const y = Math.round((e.clientY - rect.top));
+{ x:212, y:100, type:"+2" },
+{ x:358, y:93,  type:"scandal" },
+{ x:498, y:92,  type:"+3" },
+{ x:638, y:106, type:"+5" },
+{ x:805, y:102, type:"loseAll" },
 
-  points.push({x,y});
-
-  drawPoints();
-
-  document.getElementById("coords").innerText =
-    JSON.stringify(points,null,2);
-});
+{ x:925, y:129, type:"halfSkip" },
+{ x:923, y:238, type:"+3
